5b03572e9883c807689af878448c656c5f9e5f7b
DEEP DEBNATH <130180839+oggydeep@users.noreply.github.com>
2026-07-29 22:04:07 +0530

    Update Blinkit_SQL_data_analysis.sql

--- Zepto_SQL_data_analysis.sql
+++ Zepto_SQL_data_analysis.sql
@@ -1,6 +1,6 @@
-drop table if exists zepto;
+drop table if exists blinkit;
 
-create table zepto (
+create table blinkit (
 sku_id SERIAL PRIMARY KEY,
 category VARCHAR(120),
 name VARCHAR(150) NOT NULL,
@@ -16,14 +16,14 @@ quantity INTEGER
 --data exploration
 
 --count of rows
-select count(*) from zepto;
+select count(*) from blinkit;
 
 --sample data
-SELECT * FROM zepto
+SELECT * FROM blinkit
 LIMIT 10;
 
 --null values
-SELECT * FROM zepto
+SELECT * FROM blinkit
 WHERE name IS NULL
 OR
 category IS NULL
@@ -44,17 +44,17 @@ quantity IS NULL;
 
 --different product categories
 SELECT DISTINCT category
-FROM zepto
+FROM blinkit
 ORDER BY category;
 
 --products in stock vs out of stock
 SELECT outOfStock, COUNT(sku_id)
-FROM zepto
+FROM blinkit
 GROUP BY outOfStock;
 
 --product names present multiple times
 SELECT name, COUNT(sku_id) AS "Number of SKUs"
-FROM zepto
+FROM blinkit
 GROUP BY name
 HAVING count(sku_id) > 1
 ORDER BY count(sku_id) DESC;
@@ -62,51 +62,51 @@ ORDER BY count(sku_id) DESC;
 --data cleaning
 
 --products with price = 0
-SELECT * FROM zepto
+SELECT * FROM blinkit
 WHERE mrp = 0 OR discountedSellingPrice = 0;
 
-DELETE FROM zepto
+DELETE FROM blinkit
 WHERE mrp = 0;
 
 --convert paise to rupees
-UPDATE zepto
+UPDATE blinkit
 SET mrp = mrp / 100.0,
 discountedSellingPrice = discountedSellingPrice / 100.0;
 
-SELECT mrp, discountedSellingPrice FROM zepto;
+SELECT mrp, discountedSellingPrice FROM blinkit;
 
 --data analysis
 
 -- Q1. Find the top 10 best-value products based on the discount percentage.
 SELECT DISTINCT name, mrp, discountPercent
-FROM zepto
+FROM blinkit
 ORDER BY discountPercent DESC
 LIMIT 10;
 
 --Q2.What are the Products with High MRP but Out of Stock
 
 SELECT DISTINCT name,mrp
-FROM zepto
+FROM blinkit
 WHERE outOfStock = TRUE and mrp > 300
 ORDER BY mrp DESC;
 
 --Q3.Calculate Estimated Revenue for each category
 SELECT category,
 SUM(discountedSellingPrice * availableQuantity) AS total_revenue
-FROM zepto
+FROM blinkit
 GROUP BY category
 ORDER BY total_revenue;
 
 -- Q4. Find all products where MRP is greater than ₹500 and discount is less than 10%.
 SELECT DISTINCT name, mrp, discountPercent
-FROM zepto
+FROM blinkit
 WHERE mrp > 500 AND discountPercent < 10
 ORDER BY mrp DESC, discountPercent DESC;
 
 -- Q5. Identify the top 5 categories offering the highest average discount percentage.
 SELECT category,
 ROUND(AVG(discountPercent),2) AS avg_discount
-FROM zepto
+FROM blinkit
 GROUP BY category
 ORDER BY avg_discount DESC
 LIMIT 5;
@@ -114,7 +114,7 @@ LIMIT 5;
 -- Q6. Find the price per gram for products above 100g and sort by best value.
 SELECT DISTINCT name, weightInGms, discountedSellingPrice,
 ROUND(discountedSellingPrice/weightInGms,2) AS price_per_gram
-FROM zepto
+FROM blinkit
 WHERE weightInGms >= 100
 ORDER BY price_per_gram;
 
@@ -124,11 +124,11 @@ CASE WHEN weightInGms < 1000 THEN 'Low'
 	WHEN weightInGms < 5000 THEN 'Medium'
 	ELSE 'Bulk'
 	END AS weight_category
-FROM zepto;
+FROM blinkit;
 
 --Q8.What is the Total Inventory Weight Per Category 
 SELECT category,
 SUM(weightInGms * availableQuantity) AS total_weight
-FROM zepto
+FROM blinkit
 GROUP BY category
 ORDER BY total_weight;
